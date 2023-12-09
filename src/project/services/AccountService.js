00ea31ec6773e44e17e2b985b5df4312327ffd15
Erika Ding <erikajding@gmail.com>
2023-12-09 05:00:59 -0500

    Fixed cookies in session

--- src/project/services/AccountService.js
+++ src/project/services/AccountService.js
@@ -1,35 +1,39 @@
 import axios from "axios";
 
+const request = axios.create({
+  withCredentials: true,
+});
+
 const ACCOUNTS_URL = "http://localhost:4000/api/users";
 
 export const createAccount = async (account) => {
   try {
-    const response = await axios.post(`${ACCOUNTS_URL}/register`, account);
+    const response = await request.post(`${ACCOUNTS_URL}/register`, account);
     return response.data;
   } catch (err) {
     console.log(err);
   }
 };
 export const account = async () => {
-  const response = await axios.post(`${ACCOUNTS_URL}/account`);
+  const response = await request.post(`${ACCOUNTS_URL}/account`);
   return response.data;
 };
 export const signup = async (credentials) => {
-  const response = await axios.post(`${ACCOUNTS_URL}/register`, credentials);
+  const response = await request.post(`${ACCOUNTS_URL}/register`, credentials);
   return response.data;
 };
 export const signout = async () => {
-  const response = await axios.post(`${ACCOUNTS_URL}/signout`);
+  const response = await request.post(`${ACCOUNTS_URL}/signout`);
   return response.data;
 };
 export const updateUser = async (user) => {
-  const response = await axios.put(`${ACCOUNTS_URL}/${user.username}`, user);
+  const response = await request.put(`${ACCOUNTS_URL}/${user.username}`, user);
   return response.data;
 };
 
 export const loginAccount = async (userData) => {
   try {
-    const response = await axios.post(`${ACCOUNTS_URL}/login`, userData);
+    const response = await request.post(`${ACCOUNTS_URL}/login`, userData);
     console.log("test");
     return response.data;
   } catch (err) {
@@ -39,7 +43,7 @@ export const loginAccount = async (userData) => {
 
 export const getAccount = async (username) => {
   try {
-    const response = await axios.get(`${ACCOUNTS_URL}/${username}`);
+    const response = await request.get(`${ACCOUNTS_URL}/${username}`);
     return response.data;
   } catch (err) {
     console.log("couldn't find");
