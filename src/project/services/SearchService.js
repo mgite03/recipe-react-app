62e99b1d35f3c1c7f06bf43572b084ef76343571
Meghna Gite <meghnagite@gmail.com>
2023-12-09 17:46:19 -0500

    oops key leak

--- src/project/services/SearchService.js
+++ src/project/services/SearchService.js
@@ -2,15 +2,18 @@ import axios from "axios";
 
 // const KEY = process.env.REACT_APP_NAPSTER_API_KEY;
 // const KEY = "551137adc6msh83e1d1c10073965p161b45jsn5e87f4d68d8e";
-const KEY = "bdca2962e7msh49217335710b68fp1052c1jsn504f42d54e5e";
+const KEY = process.env.API_KEY;
 
 
 const options = (text) => {
     return {
     method: 'GET',
-    url: 'https://tasty.p.rapidapi.com/recipes/auto-complete',
+    url: 'https://tasty.p.rapidapi.com/recipes/list',
     params: {
-      prefix: text
+      // prefix: text
+      from: '0',
+      size: '20',
+      tags: 'under_30_minutes'
     },
     headers: {
       'X-RapidAPI-Key': KEY,
